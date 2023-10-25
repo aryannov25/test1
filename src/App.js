06f736c84fcd871b51d70756e3ab2ce56fb2f3c8
Aryan Chaudhary <aryannov25@gmail.com>
2023-10-25 13:40:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <input
-        value={xAxis}
-        onChange={(e) => setXAxis(e.target.value)}
-      />
-      <input
-        placeholder="Y-axis"
-        value={yAxis}
-        onChange={(e) => setYAxis(e.target.value)}
-      />
+      <input value={xAxis} onChange={(e) => setXAxis(e.target.value)} />
+      <input value={yAxis} onChange={(e) => setYAxis(e.target.value)} />
       <a href={submit}>
         <button type="submit">Submit</button>
       </a>
